f012aa3da8fdd4dd7961ed6f240250d4435fe374
LJMcMenamin <2083235m@student.gla.ac.uk>
2016-11-03 16:19:06 +0000

    Add files via upload

--- RaDecAl.m
+++ RaDecAl.m
@@ -1,7 +1,6 @@
 function [ source] = RaDecAl(N)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-
+%Want to eventually use this function to create RA and dec combinations for
+%the who sky
 
 source.alpha = 2*pi*rand(N, 1);
 source.delta = -(pi/2) + acos(2*rand(N, 1) - 1);
