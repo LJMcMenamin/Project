0d29f8bef1e83b3fb47d83413bfa5b7d5584ffc9
LJMcMenamin <2083235m@student.gla.ac.uk>
2016-11-03 16:17:57 +0000

    Add files via upload

--- LinCom.m
+++ LinCom.m
@@ -10,6 +10,8 @@ function [ newemitdt ] = LinCom( emitdt, RB_matrix )
 spots = round(linspace(1,length(emitdt),m)); 
 A = emitdt(spots);
 
+%Invert A so that the SoLE can be solved, then solve and find the
+%coefficients which make emitdt from RB_matrix
 B = A'
 C = RB_matrix(:, 1:m);
 coefficients= B\C;
@@ -18,7 +20,14 @@ coefficients= B\C;
 
 %% We then want to build emitdt using the coefficients found
 
-newemitdt = interp1(B,  ,RB_matrix, 
+%First
+%Build an m-long version of emitdt from RB_matrix and the coefficients
+first = coefficients*RB_matrix;
+
+%Second
+%Somehow try to recreate entire emitdt from this
+
+newemitdt = interp1(RB_matrix,1:1000, 'spline');
 
 end
 
